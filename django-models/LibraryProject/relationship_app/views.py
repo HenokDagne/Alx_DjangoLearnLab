7693552cd02991453ddcb87df7b63c85faa6e84c
henok dagne <henokdagne19@gmail.com>
2025-07-19 21:55:16 +0300

    update  User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from .models import Library
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
+from django.contrib.auth import login
 
 
 def list_books(request):
