187a5457c8aeb821ad2ef373aa549c5a775169d8
henok dagne <henokdagne19@gmail.com>
2025-07-26 18:51:25 +0300

    add bookshelf/forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,19 +1,8 @@
 from django import forms
 from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'publication_year']
-        widgets = {
-            'title': forms.TextInput(attrs={'required': True}),
-            'author': forms.TextInput(attrs={'required': True}),
-            'publication_year': forms.NumberInput(attrs={'min': 0}),
-        }
 
-    # Additional validation can be added here
-    def clean_title(self):
-        title = self.cleaned_data.get('title')
-        if not title:
-            raise forms.ValidationError('Title is required.')
-        return title
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
