23e5e782d2a4f32b6ebf523e7844e97d4d1adb2c
henok dagne <henokdagne19@gmail.com>
2025-08-03 11:05:26 +0300

    update code

--- api_project/api_project/api_project/settings.py
+++ api_project/api_project/api_project/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework', 
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework', 
-    'rest_framework.authtoken',
     'api', 
 ]
 
