0d6ea60d50d8b62f07486ec6e70a55e66a24379d
henok dagne <henokdagne19@gmail.com>
2025-08-03 16:55:59 +0300

    serializer.py

--- api_project/api_project/api/serializers.py
+++ api_project/api_project/api/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
-    class Meta: 
+    class Meta:
         model = Book
         fields = '__all__'
-        
