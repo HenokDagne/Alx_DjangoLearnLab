ac29c73c70a0e753c2760c53a566bcadb249b1d4
henok dagne <henokdagne19@gmail.com>
2025-08-03 11:30:25 +0300

    update code

--- api_project/api_project/api/views.py
+++ api_project/api_project/api/views.py
@@ -1,8 +1,19 @@
+from django.shortcuts import render
 from rest_framework import generics, viewsets
-from rest_framework.permissions import IsAuthenticated
-from .models import Book
+from rest_framework.authentication import TokenAuthentication
+from .permissions import IsAdminReadOnly
 from .serializers import BookSerializer
+from .models import Book
+
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminReadOnly]
+    
     queryset = Book.objects.all()
     serializer_class = BookSerializer
