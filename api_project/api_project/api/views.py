153f5c2b09a343a2e28d959416a7065ab40a2508
henok dagne <henokdagne19@gmail.com>
2025-08-03 11:22:22 +0300

    update code

--- api_project/api_project/api/views.py
+++ api_project/api_project/api/views.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
