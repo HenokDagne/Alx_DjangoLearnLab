5017979c9708b97d1cf3dc0c3e4ff0efa2c10737
henok dagne <henokdagne19@gmail.com>
2025-08-09 22:00:33 +0300

    change in generic view

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,46 +1,21 @@
-from rest_framework import mixins, generics
-from .models import Book, Author
+from rest_framework import generics
+from .models import Book
 from .serializers import BookSerializer
 
-# List all books & create new books
-class BookListCreateView(mixins.ListModelMixin,
-                         mixins.CreateModelMixin,
-                         generics.GenericAPIView):
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    # GET -> list all
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    # POST -> create new
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-# Retrieve, update, delete a book
-class BookRetrieveUpdateDestroyView(mixins.RetrieveModelMixin,
-                                    mixins.UpdateModelMixin,
-                                    mixins.DestroyModelMixin,
-                                    generics.GenericAPIView):
+class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-    # GET -> retrieve
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    # PUT -> update full object
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    # PATCH -> partial update
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    # DELETE -> remove object
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+        
