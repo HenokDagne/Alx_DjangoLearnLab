e438a4b4b8c60b67ce6dcb24de4c7c86f8fc8186
henok dagne <henokdagne19@gmail.com>
2025-08-17 11:27:55 +0300

    Set Up User Authentication Views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
