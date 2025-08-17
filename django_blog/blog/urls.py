8d51e7d00bb7d5ecc40931ad9cf7d9f2dfcbcbdd
henok dagne <henokdagne19@gmail.com>
2025-08-17 13:32:00 +0300

    update code

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
+from .views import RegisterView, ProfileView, ProfileUpdateView
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/update/', ProfileUpdateView.as_view(), name='profile_update'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,20 +1,19 @@
-from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm, ProfileUpdateForm
 
-class ProfileView(LoginRequiredMixin, TemplateView):
-    template_name = 'profile_template/profile.html'
-
 class RegisterView(CreateView):
     form_class = CustomUserCreationForm
-    template_name = 'registration/signup.html'
+    template_name = 'signup.html'
     success_url = reverse_lazy('profile')
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'profile.html'
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     form_class = ProfileUpdateForm
-    template_name = 'profile_template/profile_update.html'
+    template_name = 'profile_update.html'
     success_url = reverse_lazy('profile')
 
     def get_object(self):
