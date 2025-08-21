f7a9e166dec7604ba7e168be11d2aa024c2d8462
henok dagne <henokdagne19@gmail.com>
2025-08-21 23:09:21 +0300

    Serializer Setup

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate, get_user_model
-from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 from .models import CustomUser
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -11,14 +10,13 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(
+        user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-        Token.objects.create(user=user)
         return user
 
 class LoginSerializer(serializers.Serializer):
--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -2,11 +2,26 @@ from rest_framework import serializers
 from posts.models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Post
         fields = ['id', 'author', 'content', 'created_at', 'updated_at']
 
+    def validate_content(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Post content cannot be empty.")
+        return value
+
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+
     class Meta:
         model = Comment
         fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
+
+    def validate_content(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Comment content cannot be empty.")
+        return value
