3a1a9b85da62d70d6375f315211ee4d20dee3491
henok dagne <henokdagne19@gmail.com>
2025-08-21 21:52:38 +0300

    add add this file for serialzing purpose accounts/serializers.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,42 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
 from .models import CustomUser
-from rest_framework import serializers
-
-# Serializers
-class RegisterSerializer(serializers.ModelSerializer):
-	password = serializers.CharField(write_only=True)
-
-	class Meta:
-		model = CustomUser
-		fields = ('username', 'email', 'password', 'bio', 'profile_picture')
-
-	def create(self, validated_data):
-		user = CustomUser.objects.create_user(
-			username=validated_data['username'],
-			email=validated_data.get('email', ''),
-			password=validated_data['password'],
-			bio=validated_data.get('bio', ''),
-			profile_picture=validated_data.get('profile_picture', None)
-		)
-		return user
-
-class LoginSerializer(serializers.Serializer):
-	username = serializers.CharField()
-	password = serializers.CharField(write_only=True)
-
-	def validate(self, data):
-		user = authenticate(username=data['username'], password=data['password'])
-		if user and user.is_active:
-			return user
-		raise serializers.ValidationError("Invalid credentials")
-
-class ProfileSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = CustomUser
-		fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'following')
+from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
 
 # Views
 class RegisterView(generics.CreateAPIView):
