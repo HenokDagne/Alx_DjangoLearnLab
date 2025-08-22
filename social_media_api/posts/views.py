78a9e1844b745f8b6f3b0a685b84720691ee0ce7
henok dagne <henokdagne19@gmail.com>
2025-08-22 13:34:57 +0300

    Implement the Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, permissions
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Post, Comment
@@ -36,7 +36,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 
 class FeedView(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         # Get users that the current user is following
