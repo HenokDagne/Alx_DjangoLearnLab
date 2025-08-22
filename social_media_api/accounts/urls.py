a5127367dbf59e22cd4dfe4d9e35ea26e40ecb6d
henok dagne <henokdagne19@gmail.com>
2025-08-22 13:38:16 +0300

    Define URL Patterns for New Features

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 	path('profile/', ProfileView.as_view(), name='profile'),
     path('follow/<int:pk>/', FollowUserView.as_view(), name='follow-user'),
     path('unfollow/<int:pk>/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user-alt'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user-alt'),
 ]
